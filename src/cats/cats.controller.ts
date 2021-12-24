7a53ed325dbcaec82341d92abc14688ea5cba892
Kevin <kevinsevinche@gmail.com>
2021-12-24 14:10:25 -0500

    Create and add a DTO schema

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,36 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { Request } from 'express';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
-  @Get()
+  @Post()
+  @HttpCode(201)
+  async create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
+  @Get('ab*cd')
   findAll(@Req() request: Request): string {
     return 'This action returns all cats';
+  }
+
+  // @Get(':id')
+  // findOne(@Param() params): string {
+  //   console.log(params.id);
+  //   return `This action returns a #${params.id} cat`;
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
   }
 }
